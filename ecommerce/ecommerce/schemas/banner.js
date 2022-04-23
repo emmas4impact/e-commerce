e74cfb9861497e11c462568fe1a283cffb977d5b
Emmanuel Adedeji <emmans4destiny@gmail.com>
2022-04-24 01:33:17 +0300

    e-commerce using sanity

---
+++ ecommerce/ecommerce/schemas/banner.js
@@ -0,0 +1,57 @@
+export default {
+  name: "banner",
+  title: "Banner",
+  type: "document",
+  fields: [
+    {
+      name: "image",
+      title: "Image",
+      type: "array",
+      of: [{ type: "image" }],
+      Option: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "buttonText",
+      title: "ButtonText",
+      type: "string",
+    },
+
+    {
+      name: "desc",
+      title: "Desc",
+      type: "string",
+    },
+    {
+      name: "smallText",
+      titile: "SmallText",
+      type: "string",
+    },
+    {
+      name: "midText",
+      titile: "MidText",
+      type: "string",
+    },
+    {
+      name: "largeText1",
+      titile: "LargeText1",
+      type: "string",
+    },
+    {
+      name: "largeText2",
+      titile: "LargeText2",
+      type: "string",
+    },
+    {
+      name: "discount",
+      titile: "Discount",
+      type: "string",
+    },
+    {
+      name: "saleTime",
+      titile: "SaleTime",
+      type: "string",
+    },
+  ],
+};
