615e7af03a52a24c9bf2a4a4c1a9e645219da3df
Emmanuel Adedeji <emmans4destiny@gmail.com>
2022-04-24 01:39:38 +0300

    css styling

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      HeroBanner
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+    </>
   )
 }
 
-export default index
+export default Home
