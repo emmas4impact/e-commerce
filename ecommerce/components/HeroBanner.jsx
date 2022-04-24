08fd81d70af8f3493286b59106727210085d3e1a
Emmanuel Adedeji <emmans4destiny@gmail.com>
2022-04-24 21:10:37 +0300

    component boiler plate

---
+++ ecommerce/components/HeroBanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className='beats-solo'>Small Text</p>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
