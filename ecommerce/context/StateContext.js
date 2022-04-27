6f3495c5719e928e5d1bb717b50d49f5bca8d0fb
Emmanuel Adedeji <emmans4destiny@gmail.com>
2022-04-27 13:21:06 +0300

    Update StateContext.js

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -5,10 +5,34 @@ const Context = createContext();
 
 export const StateContext =({children})=>{
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] =useState();
+    const [cartItems, setCartItems] =useState([]);
     const [totalPrice, setTotalPrice] =useState();
-    const [totalQuantites, setTotalQuantities]= useState();
+    const [totalQuantities, setTotalQuantities]= useState(0);
     const [qty, setQty]= useState(1);
+
+    const onAdd =(product, quantity) =>{
+        const checkProductInCart = cartItems.find((item)=>item._id === product._id);
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+        if(checkProductInCart){
+
+            const updateCartItems = cartItems.map((cartProduct) =>{
+                if(cartProduct._id === product._id) return{
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+            setCartItems(updateCartItems);
+            
+        }else{
+            product.quantity = quantity;
+            
+            setCartItems([...cartItems, {...product}])
+        }
+        toast.success(`${qty} ${product.name} added to the cart.`)
+    }
 
     const incQty = ()=>{
         setQty((preQty) => preQty + 1);
@@ -26,12 +50,14 @@ export const StateContext =({children})=>{
     return(
         <Context.Provider value={{
             showCart,
+            setShowCart,
             cartItems,
             totalPrice,
-            totalQuantites,
+            totalQuantities,
             qty,
             incQty,
-            decQty
+            decQty,
+            onAdd
 
         }}>
           
@@ -39,3 +65,4 @@ export const StateContext =({children})=>{
         </Context.Provider>
     )
 }
+export const useStateContext= ()=>useContext(Context);
